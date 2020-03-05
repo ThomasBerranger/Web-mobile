4adf75aa042dfb4a0ebc7b2fe0c63f82798982bd
ThomasBerranger <tberranger@hotmail.fr>
2020-03-05 11:00:19 +0100

    animated menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBars, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faSearch, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 import styled from 'styled-components'
+import { motion } from 'framer-motion'
 
 const Header = () => {
+  const [menu, setMenu] = useState('hidden')
   return (
     <div>
+      <Menu animate={menu} variants={menuVariants}>
+        <ContentMenuStyled>
+          <TitleStyled>
+            <Link to={`/`}>leboncoin</Link>
+          </TitleStyled>
+          <br />
+          <br />
+          Link
+          <br />
+          Link
+          <br />
+          Link
+        </ContentMenuStyled>
+        <CloseMenuStyled>
+          <FontAwesomeIcon
+            icon={faTimes}
+            size='2x'
+            onClick={() => {
+              setMenu(menu === 'visible' ? 'hidden' : 'visible')
+            }}
+          />
+        </CloseMenuStyled>
+      </Menu>
       <HeaderStyled>
         <MenuStyled>
-          <FontAwesomeIcon icon={faBars} size='2x' />
+          <FontAwesomeIcon
+            icon={faBars}
+            size='2x'
+            onClick={() => {
+              setMenu(menu === 'visible' ? 'hidden' : 'visible')
+            }}
+          />
         </MenuStyled>
 
         <TitleStyled>
@@ -25,6 +56,33 @@ const Header = () => {
     </div>
   )
 }
+
+const Menu = styled(motion.div)`
+  width: 100%;
+  height: 100vh;
+  background-color: #fff;
+  position: absolute;
+  top: 0;
+  z-index: 2;
+  display: inline-flex;
+`
+
+const menuVariants = {
+  visible: {
+    x: 0
+  },
+  hidden: { x: -document.body.clientWidth }
+}
+
+const ContentMenuStyled = styled.div`
+  padding: 5%;
+  width: 80%;
+`
+
+const CloseMenuStyled = styled.div`
+  padding: 7% 5% 5% 5%;
+  width: 10%;
+`
 
 const HeaderStyled = styled.header`
   color: #f56b2a !important;
