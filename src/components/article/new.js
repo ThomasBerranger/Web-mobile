f1717a044eb9cd2fea8679cffa10503f0dc11c9c
ThomasBerranger <tberranger@hotmail.fr>
2020-03-23 19:44:02 +0100

    location redirect after submit

--- src/components/article/new.js
+++ src/components/article/new.js
@@ -10,6 +10,7 @@ const NewArticle = () => {
   const onSubmit = values => {
     localStorage.setItem(articleNumber, JSON.stringify(values))
     articleNumber++
+    window.location = '/articles/Ile-de-France'
   }
 
   return (
@@ -18,34 +19,39 @@ const NewArticle = () => {
       <FormStyled onSubmit={handleSubmit(onSubmit)}>
         <InputStyled name='id' type='hidden' value={articleNumber} />
 
+        <LabelStyled>Nom de l'annonce</LabelStyled>
         <InputStyled
           name='name'
           ref={register({
             required: 'Required'
           })}
         />
-
+        <br />
+        <LabelStyled>Prix de l'article</LabelStyled>
         <InputStyled
           name='price'
           ref={register({
             required: 'Required'
           })}
         />
-
+        <br />
+        <LabelStyled>URL d'image en ligne</LabelStyled>
         <InputStyled name='image_url' ref={register({})} />
-
+        <br />
+        <LabelStyled>Catégorie</LabelStyled>
         <InputStyled name='category' placeholder='Jeux' ref={register({})} />
-
+        <br />
+        <LabelStyled>Région</LabelStyled>
         <InputStyled
           name='location'
           placeholder='Paris 75000'
           ref={register({})}
         />
-
+        <br />
         <InputStyled
           name='upload_date'
           type='hidden'
-          value="aujourd\'hui"
+          value="aujourd'hui"
           ref={register({})}
         />
 
@@ -59,6 +65,10 @@ const FormStyled = styled.form`
   width: 100%;
   margin-top: 25%;
   text-align: center;
+`
+
+const LabelStyled = styled.label`
+  padding: 0 50px;
 `
 
 const InputStyled = styled.input`
